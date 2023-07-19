1ad8e261d48a9a2fb3e216204ac9cc1173977ff1
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-07-19 23:28:53 +0900

    admin

--- api/admin.py
+++ api/admin.py
@@ -30,7 +30,7 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
-admin.site.resister(models.User, UserAdmin)
+admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Profile)
 admin.site.register(models.Post)
 admin.site.register(models.Comment)
