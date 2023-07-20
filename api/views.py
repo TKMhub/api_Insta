57120e2004d636d2df06b343f6d08bd70bed80e4
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-07-20 23:17:33 +0900

    views

--- api/views.py
+++ api/views.py
@@ -4,8 +4,39 @@ from rest_framework.permissions import AllowAny
 from . import serializers
 from .models import Profile, Post, Comment
 
+
 class CreateUserView(generics.CreateAPIView):
     serializer_class = serializers.UserSerializer
     permission_classes = (AllowAny,)
 
 
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializers_classes = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializers.save(userProfile=self.request.user)
+
+
+class MyProfileListView(generics.ListAPIVIEW):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.object.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userPost=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userCommit=self.request.user)
