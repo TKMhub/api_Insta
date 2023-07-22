812e4527a50614094422fe504811469ca6155706
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-07-23 00:36:53 +0900

    修正

--- api/serializers.py
+++ api/serializers.py
@@ -31,7 +31,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'userPost', 'created_on', 'img', 'liked')
+        fields = ('id', 'title', 'userPost', 'create_on', 'img', 'liked')
         extra_kwargs = {'userPost': {'read_only': True}}
 
 
